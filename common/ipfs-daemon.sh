78d5b18bd3de5153456cf8a3e1a4634f88053c1d
adamcanray <adamcool180@gmail.com>
2024-01-30 13:22:53 +0700

    Start ipfs service with --dry-run

--- common/ipfs-daemon.sh
+++ common/ipfs-daemon.sh
@@ -6,5 +6,8 @@ mkdir -p /run/openrc || true
 touch /run/openrc/softlevel
 rc-update add ipfs default
 rc-service ipfs start --dry-run
+# rc-service ipfs start
+sleep 1
+rc-status -a
 
 echo "[ipfs-daemon] IPFS Daemon Started"
