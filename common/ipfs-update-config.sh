2b4208665d36bf4922b556feecb774f548bce37a
adamcanray <adamcool180@gmail.com>
2024-02-15 12:59:35 +0700

    not expose the API server (:5001)

--- common/ipfs-update-config.sh
+++ common/ipfs-update-config.sh
@@ -11,7 +11,8 @@ mkdir -p /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs
 cp ~/.ipfs/config /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/config
 
 # Update the Addresses.API and Addresses.Gateway fields in the config replica file
-jq --arg ip_address "$CURRENT_NODE_IP_ADDRESS" '.Addresses.API = ["/ip4/\($ip_address)/tcp/5001", "/ip6/::/tcp/5001"] | .Addresses.Gateway = ["/ip4/\($ip_address)/tcp/8080", "/ip6/::/tcp/8080"]' /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/config > /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/updated-config
+# API server is not exposed to the public network
+jq --arg ip_address "$CURRENT_NODE_IP_ADDRESS" '.Addresses.API = ["/ip4/127.0.0.1/tcp/5001", "/ip6/::/tcp/5001"] | .Addresses.Gateway = ["/ip4/\($ip_address)/tcp/8080", "/ip6/::/tcp/8080"]' /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/config > /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/updated-config
 
 # Overwrite the original config file with the updated one
 cat /vol/swarm/$CURRENT_NODE_IP_ADDRESS/ipfs/updated-config > ~/.ipfs/config
